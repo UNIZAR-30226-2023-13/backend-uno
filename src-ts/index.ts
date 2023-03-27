fad8a82bfbceef678f453f8cd1009e210f78aca3
juan-catalan <juancatalan2001@gmail.com>
2023-03-27 15:36:11 +0200

    Añadir datos para la interfaz de Session

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -1,14 +1,30 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cookieSession from "cookie-session";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+declare module 'express-session' {
+  interface SessionData {
+    username: string;
+    loggeado: boolean;
+  }
+}
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cookieSession({ keys: ["mykey"] }));
+
+
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server (is running)');
 });
+
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
