a58ee13af64a1a5cab8ebf40497bcd6b34aadc1d
juan-catalan <juancatalan2001@gmail.com>
2023-05-31 14:00:56 +0200

    Añadir servicio para registrarse y obtener usuarios conectados

---
+++ src-ts/services/usuariosConectados.service.ts
@@ -0,0 +1,14 @@
+const jugadoresSocket: Map<string, string> = new Map<string, string>();
+
+export function conectarJugador(username: string, socket: string) {
+    jugadoresSocket.set(socket, username);
+}
+
+export function desconectarJugador(socket: string) {
+    jugadoresSocket.delete(socket);
+}
+
+export function jugadoresConectados(): string[] {
+    const jugadores = Array.from(jugadoresSocket.values());
+    return jugadores;
+}
