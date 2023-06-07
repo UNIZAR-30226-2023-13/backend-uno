7ae8b704af6890e97bc164e8c6818238fdae7585
juan-catalan <juancatalan2001@gmail.com>
2023-06-07 18:52:39 +0200

    Cambio a secureProxy

--- src-ts/index.ts
+++ src-ts/index.ts
@@ -1,13 +1,16 @@
 import express, { Express, Request, RequestHandler, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import { mensajeHandler } from "./handlers/message.socket";
+import { connectionHandler } from "./handlers/connectionHandler";
+import { desconnectionHandler } from "./handlers/desconnectionHandler";
+import { partidaHandler } from "./handlers/partidaHandler";
+
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
 import { Socket, Server as SocketIOServer } from "socket.io";
 import http = require("http");
-import { Tablero } from "./models/tablero";
-import { Jugador } from "./models/jugador";
 
 import partidasRouter = require("./routes/partidas.route");
 import loginRoute = require("./routes/login.route");
@@ -59,7 +62,7 @@ if (process.env.ENVIROMENT === "DEV") {
             maxAge: 1000 * 60 * 60 * 24,
             keys: ["mykey"],
             httpOnly: false,
-            secure: true,
+            secureProxy: true,
             sameSite: "none",
         })
     );
@@ -79,48 +82,11 @@ app.use("/partidas", auth as RequestHandler, partidasRouter as RequestHandler);
 
 app.get("/", auth as RequestHandler, (req: Request, res: Response) => {
     res.send("Express + TypeScript Server (is running)");
-    // Esto es para rolear que existen partidas
-    // Para añadir una cambiar el nombre de jugadores y acceder desde el navegador
-    // a la ruta / del server
-    const jugador1: Jugador = {
-        username: "juan",
-        puntos: 0,
-        mano: [{ numero: 1, color: "rojo" }],
-    };
-    const jugador2: Jugador = {
-        username: "adrian",
-        puntos: 0,
-        mano: [],
-    };
-    const jugador3: Jugador = {
-        username: "claudia",
-        puntos: 0,
-        mano: [{ numero: 1, color: "rojo" }],
-    };
-    const jugador4: Jugador = {
-        username: "javier",
-        puntos: 0,
-        mano: [],
-    };
-    const partida: Tablero = new Tablero(
-        [],
-        [],
-        [jugador1, jugador2, jugador3, jugador4],
-        true
-    );
-    partida.ganador = jugador1;
-    anadirPartida(partida);
 });
 
 server.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
-
-import { mensajeHandler } from "./handlers/message.socket";
-import { connectionHandler } from "./handlers/connectionHandler";
-import { desconnectionHandler } from "./handlers/desconnectionHandler";
-import { partidaHandler } from "./handlers/partidaHandler";
-import { anadirPartida } from "./services/historialPartidas.service";
 
 io.on("connection", (socket: Socket) => {
     // Handler de la conexion
